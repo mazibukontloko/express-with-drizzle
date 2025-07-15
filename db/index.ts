763b17abaa06ab37f0e25584d6fc75eee5709147
koks <mazibukontloko@yahoo.com>
2025-07-15 20:17:48 +0200

    Merge branch 'do-drizzle'

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 
-import { drizzle, BetterSQLite3Database } from "drizzle-orm/better-sqlite3";
+import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
 
 const sqlite = new Database('sqliteI.db');
